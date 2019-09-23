b093ec4d360d7d6c77595252c68b9685e65d9011
RashidaBengali <bengali.rashida@gmail.com>
2019-09-23 23:03:00 +1000

    added mouseover animation to the skill section

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(document).ready( () => {
       $('#scroll-contact').addClass('scroll-color');
     }
     else if (wS > (hT+hH-wH)) {
-      $('#scroll-skills').addClass('scroll-color');;
+      $('#scroll-skills').addClass('scroll-color');
     }
     else if (wS > (hT2+hH2-wH)){
       $('#scroll-work').addClass('scroll-color');
@@ -26,37 +26,11 @@ $(document).ready( () => {
     }
   });
 
-  //  $(window).scroll( () => {
-  //   const hT1 = $('#about').offset().top,
-  //       hH1 = $('#about').outerHeight(),
-  //       wH = $(window).height(),
-  //       wS = $(this).scrollTop();
-  //        $('#scroll-about').removeClass('scroll-color');
-  //   if (wS > (hT1+hH1-wH)){
-  //       $('#scroll-about').addClass('scroll-color');
-  //   }
-  // });
-  //
-  // $(window).scroll( () => {
-  //  const hT2 = $('#work').offset().top,
-  //      hH2 = $('#work').outerHeight(),
-  //      wH = $(window).height(),
-  //      wS = $(this).scrollTop();
-  //       $('#scroll-work').removeClass('scroll-color');
-  //  if (wS > (hT2+hH2-wH)){
-  //      $('#scroll-work').addClass('scroll-color');
-  //  }
-  // });
-  //
-  // $(window).scroll( () => {
-  //  const hT3 = $('#contact').offset().top,
-  //      hH3 = $('#contact').outerHeight(),
-  //      wH = $(window).height(),
-  //      wS = $(this).scrollTop();
-  //       $('#scroll-contact').removeClass('scroll-color');
-  //  if (wS > (hT3+hH3-wH)){
-  //      $('#scroll-contact').addClass('scroll-color');
-  //  }
-  // });
+  $("#skills").mouseover(function(){
+     $('#scroll-skills').addClass('scroll-color');
+   });
+   $("#skills").mouseout(function(){
+     $('#scroll-skills').addClass('scroll-color');
+   });
 
 });
