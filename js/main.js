0a915ba096cd07d1b1b7d34ee65b54198e7e8a09
RashidaBengali <bengali.rashida@gmail.com>
2019-09-23 17:00:42 +1000

    adding animations

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-$(document).ready(function () {
-  $('nav a').click(function () {
-    $('a').removeClass('active')
-    console.log($('a'));
-    $(this).addClass('active')
-    console.log($('a'));
-  })
-
-  $('#skillset').scroll(function() {
-       console.log('you scrolled on social');
-    });
-  });
+// $(document).ready( () => {
+//   $(".overlay").mouseover( () => {
+//     $(this).addClass("animated fadeInDown");
+//   });
+//   $(".overlay").mouseout( () => {
+//     $(this).removeClass("animated fadeInDown");
+//   });
+// });
